0d80a14316e9ccda2db4227cf6156db68505012c
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-04 21:51:47 +0100

    Tweak runner

--- runlionlbd.py
+++ runlionlbd.py
@@ -7,9 +7,10 @@ from lionlbd import app
 from lionlbd import config
 
 
-if config.DEBUG:
-    # use_reloader=False prevents double init in debug mode
-    # (http://stackoverflow.com/a/9476701)
-    app.run(debug=True, port=config.PORT, use_reloader=False)
-else:
-    app.run(host='0.0.0.0', port=config.PORT)
+if __name__ == '__main__':
+    if config.DEBUG:
+        # use_reloader=False prevents double init in debug mode
+        # (http://stackoverflow.com/a/9476701)
+        app.run(debug=True, port=config.PORT, use_reloader=False)
+    else:
+        app.run(host='0.0.0.0', port=config.PORT)
