5500b46f75c3e4ef9e6532fae0547447dc61dae9
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-04 21:06:14 +0100

    First attempt at get_neighbours

--- lionlbd/graph.py
+++ lionlbd/graph.py
@@ -5,15 +5,74 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
+from itertools import izip
 from logging import debug, info, warn, error
 
 
 class Graph(object):
+    """In-memory Neo4j graph."""
+
     def __init__(self, nodes, edges):
         """Initialize Graph."""
+        if not nodes:
+            raise ValueError('no nodes')
+        if not edges:
+            raise ValueError('no edges')
+
         self._nodes = nodes
         self._edges = edges
+        debug('initialized Graph: {}'.format(self.stats_str()))
+
+        self.node_idx_by_id = self._create_node_idx_mapping(nodes)
+
+        self.neighbours = self._create_neighbour_sequences(
+            nodes, edges, self.node_idx_by_id)
+
+        self.edges_from = self._create_edge_sequences(
+            nodes, edges, self.node_idx_by_id)
+
+    def get_neighbours(self, id_):
+        """Get neighbours for given node."""
+        try:
+            idx = self.node_idx_by_id[id_]
+        except KeyError:
+            raise KeyError('unknown node id: {}'.format(id_))
+        result = []
+        nodes, edges = self._nodes, self._edges
+        for n_idx, e_idx in izip(self.neighbours[idx], self.edges_from[idx]):
+            result.append(nodes[n_idx])
+        return result
 
     def stats_str(self):
         """Return Graph statistics as string."""
         return '{} nodes, {} edges'.format(len(self._nodes), len(self._edges))
+
+    @staticmethod
+    def _create_node_idx_mapping(nodes):
+        """Create mapping from node ID to index in given sequence."""
+        node_idx_by_id = {}
+        for idx, node in enumerate(nodes):
+            if node.id in node_idx_by_id:
+                raise ValueError('duplicate node ID {}'.format(node.id))
+            node_idx_by_id[node.id] = idx
+        return node_idx_by_id
+
+    @staticmethod
+    def _create_neighbour_sequences(nodes, edges, idx_map):
+        """Create index-based mapping from node to neighbouring nodes."""
+        # TODO: consider arrays instead of lists
+        neighbours = [[] for _ in xrange(len(nodes))]
+        for edge in edges:
+            start, end = idx_map[edge.start], idx_map[edge.end]
+            neighbours[start].append(end)
+        return neighbours
+
+    @staticmethod
+    def _create_edge_sequences(nodes, edges, idx_map):
+        """Create index-based mapping from node to edges."""
+        # TODO: consider arrays instead of lists
+        edges_from = [[] for _ in xrange(len(nodes))]
+        for idx, edge in enumerate(edges):
+            start = idx_map[edge.start]
+            edges_from[start].append(idx)
+        return edges_from
--- lionlbd/views.py
+++ lionlbd/views.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 
 import logging
 
+from flask import abort, jsonify
+from logging import debug, info, warn, error
+
 from lionlbd import app
 from lionlbd import graph
 
@@ -11,3 +14,14 @@ from lionlbd import graph
 @app.route('/index')
 def index():
     return graph.stats_str()
+
+
+@app.route('/neighbours/<id_>')
+def get_neighbours(id_):
+    try:
+        neighbours = graph.get_neighbours(id_)
+    except KeyError, e:
+        warn(e)
+        abort(404)
+    debug('get_neighbours: {} for {}'.format(len(neighbours), id_))
+    return jsonify([n._asdict() for n in neighbours])
