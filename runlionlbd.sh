adf5152dcb3456983195451c0ce079f1dffca6d3
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-07 19:29:17 +0100

    Use complete data

--- runlionlbd.sh
+++ runlionlbd.sh
@@ -2,7 +2,9 @@
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DATADIR="$SCRIPTDIR/data/neoplasms"
+DATADIR="$SCRIPTDIR/data/complete"
+#DATADIR="$SCRIPTDIR/data/neoplasms"
+#DATADIR="$SCRIPTDIR/data/neoplasms-1p"
 NODES="$DATADIR/nodes.csv"
 EDGES="$DATADIR/edges.csv"
 
