2268391bf42c334896b27bf08645948ed32db226
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-11 20:03:54 +0100

    Tweak for local

--- runlionlbd.sh
+++ runlionlbd.sh
@@ -2,11 +2,15 @@
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DATADIR="$SCRIPTDIR/data/complete"
-#DATADIR="$SCRIPTDIR/data/neoplasms"
+
+#DATADIR="$SCRIPTDIR/data/complete"
 #DATADIR="$SCRIPTDIR/data/neoplasms-1p"
+#DATADIR="$SCRIPTDIR/data/neoplasms"
+DATADIR="$SCRIPTDIR/data/neoplasms-old"
+
 NODES="$DATADIR/nodes.csv"
-EDGES="$DATADIR/edges.csv"
+#EDGES="$DATADIR/edges.csv"
+EDGES="$DATADIR/edges-cut-0-4,16.csv"
 
 python "$SCRIPTDIR"/runlionlbd.py \
        --nodes "$NODES" \
