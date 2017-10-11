59ca1d50770c235e73897cc16b694267c91555f6
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-11 21:45:06 +0100

    _get_score_sequence, implicit year filter (~40% faster)

--- lionlbd/graph.py
+++ lionlbd/graph.py
@@ -50,7 +50,10 @@ class Graph(object):
         self.edges_from = self._create_edge_sequences(
             nodes, edges, self.node_idx_by_id)
 
-        self.edge_metrics = self._get_metrics(edges[0]) if edges else []
+        self.edge_metrics = self._get_edge_metrics(edges[0]) if edges else []
+
+        self.scores_by_metric_and_year = {}    # lazy init
+        self._get_score_sequence('count', self.max_year)    # precache (TODO)
 
         debug('initialized Graph: {}'.format(self.stats_str()))
 
@@ -67,17 +70,19 @@ class Graph(object):
         except KeyError:
             raise KeyError('unknown node id: {}'.format(id_))
 
+        metric = self._validate_metric(metric)
         year = self._validate_year(year)
 
         filter_node = self._get_node_filter(types)
-        filter_edge = self._get_edge_filter(year)
         get_score = self._get_edge_scorer(metric, year)
 
         scores, n_indices = [], []
-        for n_idx, e_idx in izip(self.neighbours[idx], self.edges_from[idx]):
-            if filter_node(n_idx) or filter_edge(e_idx):
+        neighbours = self.neighbours
+        scores_from = self._get_score_sequence(metric, year)
+        for n_idx, e_score in izip(self.neighbours[idx], scores_from[idx]):
+            if filter_node(n_idx):
                 continue
-            scores.append(get_score(e_idx))
+            scores.append(e_score)
             n_indices.append(n_idx)
 
         argsorted = reversed(argsort(scores))    # TODO: argpartition if limit?
@@ -108,13 +113,13 @@ class Graph(object):
         except KeyError:
             raise KeyError('unknown node id: {}'.format(id_))
 
+        metric = self._validate_metric(metric)
         year = self._validate_year(year)
 
         agg = self._get_agg_function('avg')    # TODO
         acc = self._get_acc_function('max')    # TODO
 
         filter_node = self._get_node_filter(types)
-        filter_edge = self._get_edge_filter(year)
         get_score = self._get_edge_scorer(metric, year)
 
         nodes = self._nodes
@@ -135,15 +140,14 @@ class Graph(object):
         score = array('f', [0]) * len(nodes)
         is_c_idx = array('b', [0]) * len(nodes)
 
-        neighbours, edges_from = self.neighbours, self.edges_from
-        for b_idx, e1_idx in izip(neighbours[a_idx], edges_from[a_idx]):
-            if filter_node(b_idx) or filter_edge(e1_idx):
+        neighbours = self.neighbours
+        scores_from = self._get_score_sequence(metric, year)
+        for b_idx, e1_score in izip(neighbours[a_idx], scores_from[a_idx]):
+            if filter_node(b_idx):
                 continue
-            e1_score = get_score(e1_idx)
-            for c_idx, e2_idx in izip(neighbours[b_idx], edges_from[b_idx]):
-                if (exclude_idx[c_idx] or filter_edge(e2_idx)):
+            for c_idx, e2_score in izip(neighbours[b_idx], scores_from[b_idx]):
+                if exclude_idx[c_idx]:
                     continue
-                e2_score = get_score(e2_idx)
                 score[c_idx] = acc(score[c_idx], agg(e1_score, e2_score))
                 is_c_idx[c_idx] = True
 
@@ -160,14 +164,31 @@ class Graph(object):
             results.append(build_result(idx, score[idx]))
         return results
 
+    def get_metrics(self):
+        """Return edge weight metrics used in the graph.
+
+        Returns:
+            list of str: edge metric names.
+        """
+        return [name for index, name, type_ in self.edge_metrics]
+
     def _validate_year(self, year):
-        """Verify that given year is a valid value."""
+        """Verify that given year is valid, apply default if None."""
         if year is None:
-            return year
+            return self.max_year
         if year < self.min_year or year > self.max_year:
             # TODO: bound instead of raise?
             raise ValueError('out of bounds year {}'.format(year))
         return year
+
+    def _validate_metric(self, metric):
+        """Verify that given metric is valid, apply default if None."""
+        if metric is None:
+            return 'count'    # TODO configurable default
+        elif metric in self.get_metrics():
+            return metric
+        else:
+            raise ValueError('invalid metric {}'.format(metric))
 
     def _get_node_filter(self, types=None):
         """Return function determining whether to filter out a node."""
@@ -177,20 +198,10 @@ class Graph(object):
             node_types, types = self._tnodes.type, set(types)
             return lambda n_idx: node_types[n_idx] not in types
 
-    def _get_edge_filter(self, max_year=None):
-        """Return function determining whether to filter out an edge."""
-        if max_year is None or max_year == self.max_year:
-            return lambda e_idx: False    # filter nothing
-        else:
-            edge_year = self._tedges.year
-            return lambda e_idx: edge_year[e_idx] > max_year
-
-    def _get_edge_scorer(self, metric=None, year=None):
+    def _get_edge_scorer(self, metric, year):
         """Return function returning score for edge."""
-        if metric is None:
-            metric = 'count'    # TODO configurable default
-        if year is None:
-            year = self.max_year
+        assert metric is not None and year is not None, 'internal error'
+
         offset = year - self.max_year - 1
 
         idx_type = { m: (i, type_) for i, m, type_ in self.edge_metrics }
@@ -237,6 +248,24 @@ class Graph(object):
                 raise NotImplementedError('{}/{}'.format(degree, type_))
         else:
             raise NotImplementedError('{}/{}'.format(degree, type_))
+
+    def _get_score_sequence(self, metric, year):
+        if metric not in self.scores_by_metric_and_year:
+            self.scores_by_metric_and_year[metric] = {}
+        if year not in self.scores_by_metric_and_year[metric]:
+            # lazy init
+            info('calculating scores for metric {}, year {} ...'.format(
+                metric, year))
+            idx_map = self.node_idx_by_id
+            get_score = self._get_edge_scorer(metric, year)
+            scores_from = [[] for _ in xrange(len(self._nodes))]
+            for idx, edge in enumerate(self._edges):
+                if edge.year > year:
+                    break    # edges sorted by year
+                start = idx_map[edge.start]
+                scores_from[start].append(get_score(idx))
+            self.scores_by_metric_and_year[metric][year] = scores_from
+        return self.scores_by_metric_and_year[metric][year]
 
     def stats_str(self):
         """Return Graph statistics as string."""
@@ -323,7 +352,7 @@ class Graph(object):
         return nodes, directed_edges
 
     @staticmethod
-    def _get_metrics(edge):
+    def _get_edge_metrics(edge):
         """Return list of metric (index, name, type) tuples for namedtuple."""
         metrics = []
         for index, field in enumerate(type(edge)._fields):
