7b7712b6defb4b0bce1fb73381037eae36249fc8
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-25 10:30:25 +0300

    Add /graph/<method> route to invoke method directly

--- lionlbd/views.py
+++ lionlbd/views.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
+import json
 import logging
 
-from flask import abort, jsonify
-from logging import debug, info, warn, error
+from flask import request, abort, jsonify
+from logging import debug, info, warn, error, exception
 
 from lionlbd import app
 from lionlbd import graph
@@ -23,6 +24,46 @@ def index():
 #     from ujson import dumps
 #     return current_app.response_class((dumps(obj, indent=2), '\n'),
 #                                       mimetype='application/json')
+
+
+@app.route('/graph/<method>')
+def invoke_method(method):
+    """Invoke named method on graph."""
+    # get method on graph
+    try:
+        name, method = method, getattr(graph, method)
+    except AttributeError:
+        error('invoke_method(): no method "{}"'.format(method))
+        abort(404)
+
+    # get caller parameters
+    params = request.args.get('params')
+    try:
+        params = json.loads(params)
+    except ValueError:
+        exception('invoke_method(): failed to loads params "{}"'.format(params))
+        abort(400)
+    debug('invoke_method() params: {}'.format(params))
+
+    # convert any non-None "filters" parameter to LbdFilters (special case)
+    if params.get('filters', None) is not None:
+        try:
+            params['filters'] = graph.Filters(**params['filters'])
+        except:
+            exception('invoke_method(): failed to create filters "{}"'.format(
+                params['filters']))
+            abort(400)
+
+    # invoke the method
+    try:
+        result = method(**params)
+    except:
+        exception('invoke_method(): failed to invoke {}({})'.format(
+            name, params))
+        abort(400)
+
+    debug('invoke_method(): {} returned {}'.format(name, str(result)[:40]))
+    return jsonify(result)
 
 
 @app.route('/extend_graph/<id_>')
