11e47acaf0e4c2f8893c60501d01df2e68b3846c
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-10-31 12:18:01 +0000

    Add filters to get_nodes()

--- lionlbd/graph.py
+++ lionlbd/graph.py
@@ -342,8 +342,10 @@ class Graph(LbdInterface):
             })
         return discoverable
 
-    def get_nodes(self, ids, year=None, history=False):
+    def get_nodes(self, ids, year=None, filters=None, history=False):
         year = self._validate_year(year)
+        if filters is not None:
+            raise NotImplementedError('get_nodes() filters')
         if history:
             error('no historical data for nodes, ignoring history')
         indices = [self._get_node_idx(i) for i in ids]
