5f97d0492efa6c38a5b5bdec7832ba8f780cb841
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2018-01-27 09:34:42 +0000

    Take data directory argument

--- runlionlbd.sh
+++ runlionlbd.sh
@@ -4,6 +4,12 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DATADIR="/srv/lion/neo4j-import/complete"
 
+if [ "$#" -eq 1 ]; then
+    DATADIR="$1"
+fi
+
+echo "Using data from $DATADIR ..." >&2
+
 NODES="$DATADIR/nodes.csv"
 EDGES="$DATADIR/edges.csv"
 META="$DATADIR/meta.csv"
